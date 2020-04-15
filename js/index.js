69bca9af5a310dc208018112ece89522e6345bfd
Dazpel <alexxdav97@gmail.com>
2020-04-15 16:28:39 -0400

    name tag

--- js/index.js
+++ js/index.js
@@ -1,50 +1,56 @@
 /* Alex Starter Code */
-window.onload = () => { 
+window.onload = () => {
   introMusic.play();
 };
 
-  let introMusic = document.getElementById('start');
-  let playingMusic = document.getElementById('playing');
-  let stepMusic = document.getElementById('step');
-  let successMusic = document.getElementById('success');
-  let gameOver = document.getElementById('gameOver');
-  
-  introMusic.volume = 0.1;
-  playingMusic.volume = 0.03;
-  stepMusic.volume=0.04;
-  successMusic.volume=0.03;
-  gameOver.volume=0.3;
+let introMusic = document.getElementById('start');
+let playingMusic = document.getElementById('playing');
+let stepMusic = document.getElementById('step');
+let successMusic = document.getElementById('success');
+let gameOver = document.getElementById('gameOver');
 
+introMusic.volume = 0.1;
+playingMusic.volume = 0.03;
+stepMusic.volume = 0.04;
+successMusic.volume = 0.03;
+gameOver.volume = 0.3;
 
+lives = 3;
 let gameTracker = {
-  name: 'Pepito',
+  name: 'John Doe',
   country: 'Colombia',
   level: 1,
   hp: 100,
-  lysols: 3,
+  lives: lives,
   cFlag: '../images/colombia-flag-country.png',
   alive: true,
 };
 
-const tracker = () => {       //Update GAME TRACKER INFO
+
+
+const tracker = () => {
+  //Update GAME TRACKER INFO
   let trackDiv = document.getElementById('gameTracker');
   let initVal = `<div id="gInfo"><h1>${gameTracker.country}: Level ${gameTracker.level}/5</h1>
-  <h3>${gameTracker.name} Health: ${gameTracker.hp}pts - Lysols Left: ${gameTracker.lysols}</h3></div></div>
+  <h3>${gameTracker.name} Health: ${gameTracker.hp}pts - Lives Left: ${lives}</h3></div></div>
   <div id="gFlag"><img src="${gameTracker.cFlag}" alt=""></div> 
 </div>`;
   trackDiv.innerHTML = initVal;
-}
+};
 
 const setPlayField = () => {
   document.getElementById('explodeCovid').style.display = 'none';
   document.getElementById('mainPage').style.display = 'none';
   document.getElementById('game-board').style.display = 'grid';
-  document.getElementById('body').style.backgroundImage = `url('../images/bogota.jpg')`;
-}
+  document.getElementById(
+    'body'
+  ).style.backgroundImage = `url('../images/bogota.jpg')`;
+};
 
 const begin = () => {
-  setPlayField()    //Clear landingPage info
-  tracker();           //Init scoreboard info
+  setPlayField(); //Clear landingPage info
+  gameTracker.name = document.getElementById('name').value
+  tracker(); //Init scoreboard info
   createObstacles();
   createVirus();
   startGame();
@@ -52,12 +58,11 @@ const begin = () => {
   playingMusic.play();
 };
 
-
 /* Alex End Code */
-
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
+ctx.font = "14px Arial";
 let animateId = null;
 let newLevel = false;
 let lightCounter = 0;
@@ -65,93 +70,128 @@ let lightSwitch = false;
 let lightTime = 100;
 let canMove = true;
 let stageCounter = 0;
-let direction = ''
+let direction = '';
 let img = new Image();
 img.src = './images/sprite.png'; // Loads player
 let contamination = 0;
-lives = 3;
+
 let currentLoopIndex = 0;
 let frameCount = 0;
 let virusCount = 0;
-
 
 /* HUGO CODE */
 
 //  NEW IMAGES
 let canvasSize = {
-  x:0,
-  y:0,
-  w:700,
-  h:500,
-}
+  x: 0,
+  y: 0,
+  w: 700,
+  h: 500,
+};
 let colCanvas = new Image();
-colCanvas.src = '/images/mat to colombia.jpg'
+colCanvas.src = '/images/mat to colombia.jpg';
 let montCanvas = new Image();
-montCanvas.src = '/images/map to montenegro.jpg'
+montCanvas.src = '/images/map to montenegro.jpg';
 let grceCanvas = new Image();
-grceCanvas.src = '/images/map to greece.jpg'
+grceCanvas.src = '/images/map to greece.jpg';
 let treeImg = new Image();
-treeImg.src = '/images/tree.png'
+treeImg.src = '/images/tree.png';
 let rockImg = new Image();
-rockImg.src = '/images/rocks.png'
+rockImg.src = '/images/rocks.png';
 let mountainImg = new Image();
-mountainImg.src = '/images/mountain.png'
+mountainImg.src = '/images/mountain.png';
 let metalFloorCanvas = new Image();
-metalFloorCanvas.src = '/images/metalfloor.png'
+metalFloorCanvas.src = '/images/metalfloor.png';
 let column = new Image();
-column.src = '/images/greek-column.png'
+column.src = '/images/greek-column.png';
 
 let hugostacle = [
   {
     x: 100,
-    y: Math.floor(Math.random()* 300),
+    y: Math.floor(Math.random() * 300),
     w: 30,
-    h: 200
+    h: 200,
   },
   {
     x: 300,
-    y: Math.floor(Math.random()* 50 + 250),
+    y: Math.floor(Math.random() * 50 + 250),
     w: 30,
-    h: 200
+    h: 200,
   },
   {
     x: 450,
-    y: Math.floor(Math.random()* 300),
+    y: Math.floor(Math.random() * 300),
     w: 30,
-    h: 200
-  }
-]
+    h: 200,
+  },
+];
 function drawLog() {
-  for(i = 0; i < hugostacle.length; i++){
-  ctx.drawImage(logImg, hugostacle[i].x, hugostacle[i].y, hugostacle[i].w, hugostacle[i].h);
+  for (i = 0; i < hugostacle.length; i++) {
+    ctx.drawImage(
+      logImg,
+      hugostacle[i].x,
+      hugostacle[i].y,
+      hugostacle[i].w,
+      hugostacle[i].h
+    );
   }
 }
 function drawColumn() {
-  for(i = 0; i < hugostacle.length; i++){
-  ctx.drawImage(column, hugostacle[i].x, hugostacle[i].y, 60, hugostacle[i].h);
+  for (i = 0; i < hugostacle.length; i++) {
+    ctx.drawImage(
+      column,
+      hugostacle[i].x,
+      hugostacle[i].y,
+      60,
+      hugostacle[i].h
+    );
   }
 }
 
 function drawColCanvas() {
-  ctx.drawImage(colCanvas, canvasSize.x, canvasSize.y, canvasSize.w, canvasSize.h)
+  ctx.drawImage(
+    colCanvas,
+    canvasSize.x,
+    canvasSize.y,
+    canvasSize.w,
+    canvasSize.h
+  );
 }
 function drawMontCanvas() {
-  ctx.drawImage(montCanvas, canvasSize.x, canvasSize.y, canvasSize.w, canvasSize.h)
+  ctx.drawImage(
+    montCanvas,
+    canvasSize.x,
+    canvasSize.y,
+    canvasSize.w,
+    canvasSize.h
+  );
 }
 function drawGrceCanvas() {
-  ctx.drawImage(grceCanvas, canvasSize.x, canvasSize.y, canvasSize.w, canvasSize.h)
+  ctx.drawImage(
+    grceCanvas,
+    canvasSize.x,
+    canvasSize.y,
+    canvasSize.w,
+    canvasSize.h
+  );
 }
 function drawMetalFloor() {
-  ctx.drawImage(metalFloorCanvas, canvasSize.x, canvasSize.y, canvasSize.w, canvasSize.h)
+  ctx.drawImage(
+    metalFloorCanvas,
+    canvasSize.x,
+    canvasSize.y,
+    canvasSize.w,
+    canvasSize.h
+  );
 }
 function drawTree() {
-  ctx.drawImage(treeImg, 320, 200, 70, 90)
+  ctx.drawImage(treeImg, 320, 200, 70, 90);
 }
-function drawMountain(){
-  ctx.drawImage(mountainImg, 320, 300, 70, 90)
+function drawMountain() {
+  ctx.drawImage(mountainImg, 320, 300, 70, 90);
 }
 function drawRocks() {
-  ctx.drawImage(rockImg, 320, 30, 70, 90)
+  ctx.drawImage(rockImg, 320, 30, 70, 90);
 }
 
 /* END HUGO CODE */
@@ -187,7 +227,8 @@ function faceDirection(frameX, frameY, canvasX, canvasY) {
   player.y = canvasY;
 }
 
-function drawPlayer(){
+function drawPlayer() {
+  
   ctx.drawImage(
     img,
     player.sx,
@@ -201,83 +242,78 @@ function drawPlayer(){
   );
 }
 function step(dir) {
-
   switch (dir) {
     case 'up':
-      if(player.sx>=512){
+      if (player.sx >= 512) {
         player.sx = 0;
       }
-      faceDirection(player.sx+=64, 512, player.x, player.y);
+      faceDirection((player.sx += 64), 512, player.x, player.y);
       break;
     case 'down':
-      if(player.sx>=512){
+      if (player.sx >= 512) {
         player.sx = 0;
       }
-      faceDirection(player.sx+=64, 640, player.x, player.y);
+      faceDirection((player.sx += 64), 640, player.x, player.y);
       break;
     case 'left':
-      if(player.sx>=512){
+      if (player.sx >= 512) {
         player.sx = 0;
       }
-      faceDirection(player.sx+=64, 576, player.x, player.y);
+      faceDirection((player.sx += 64), 576, player.x, player.y);
       break;
     case 'right':
-      if(player.sx>=512){
+      if (player.sx >= 512) {
         player.sx = 0;
       }
-      faceDirection(player.sx+=64, 704, player.x, player.y);
+      faceDirection((player.sx += 64), 704, player.x, player.y);
       break;
     default:
       break;
   }
-
 }
 var object = {
-  x: Math.floor(Math.random()*550 + 50),
-  y: Math.floor(Math.random()*200 + 50),
+  x: Math.floor(Math.random() * 550 + 50),
+  y: Math.floor(Math.random() * 200 + 50),
   w: 30,
-  h: 100
-}
-let obstacle = []
-let virus = []
+  h: 100,
+};
+let obstacle = [];
+let virus = [];
 var win = {
   x: 660,
-  y: Math.floor(Math.random()*400 + 50),
+  y: Math.floor(Math.random() * 400 + 50),
   w: 40,
   h: 60,
-  type: 'win'
-}
+  type: 'win',
+};
 
 function createObstacles() {
-  for(i=0; i<3; i++)
-  {
+  for (i = 0; i < 3; i++) {
     let obs = {
-      x: 50 + Math.floor(Math.random()*125) + 200*i,
-      y: Math.floor(Math.random()*50 + 250*(i%2)),
+      x: 50 + Math.floor(Math.random() * 125) + 200 * i,
+      y: Math.floor(Math.random() * 50 + 250 * (i % 2)),
       w: 30,
-      h: 200
-    }
-    obstacle.push(obs)
+      h: 200,
+    };
+    obstacle.push(obs);
   }
 }
 function createVirus() {
-  for(i=0; i<3; i++)
-  {
+  for (i = 0; i < 3; i++) {
     let obs = {
-      x: 50 + Math.floor(Math.random()*125) + 200*i,
-      y: Math.floor(Math.random()*50 + 250*(i%2)),
+      x: 50 + Math.floor(Math.random() * 125) + 200 * i,
+      y: Math.floor(Math.random() * 50 + 250 * (i % 2)),
       w: 50,
-      h: 50
-    }
-    virus.push(obs)
+      h: 50,
+    };
+    virus.push(obs);
   }
 }
-function deleteObstacle(index)
-{
-  obstacle.splice(index,1)
+function deleteObstacle(index) {
+  obstacle.splice(index, 1);
 }
 function deleteVirus(index) {
-  virus.splice(index,1)
+  virus.splice(index, 1);
 }
 function drawObstacles(obj) {
   ctx.fillStyle = 'red';
@@ -285,7 +321,7 @@ function drawObstacles(obj) {
 }
 function drawVirusObs(obj) {
   ctx.fillStyle = 'blue';
-  ctx.fillRect(obj.x, obj.y, obj.w, obj.h)
+  ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
 }
 function winningShade() {
   ctx.fillStyle = 'blue';
@@ -300,18 +336,17 @@ function drawCanvas() {
   ctx.fillRect(0, 0, 700, 500);
 }
 function drawVirus(i) {
-  ctx.fillStyle = 'rgba(255,0,0,.25)'
-  ctx.fillRect(0,0,-25+i,500)
+  ctx.fillStyle = 'rgba(255,0,0,.25)';
+  ctx.fillRect(0, 0, -25 + i, 500);
 }
 /*function drawCar() {
   ctx.drawImage(car.image, car.x, car.y, car.w, car.h);
 }*/
 function detectMove(move) {
-  if(move)
-  {
-    switch(direction) {
+  if (move) {
+    switch (direction) {
       case 'left':
-        stepMusic.play()
+        stepMusic.play();
         if (player.x <= 0) {
           console.log('Border');
         } else {
@@ -319,7 +354,7 @@ function detectMove(move) {
         }
         break;
       case 'right':
-        stepMusic.play()
+        stepMusic.play();
         if (player.x === 660) {
           console.log('Border');
         } else {
@@ -327,7 +362,7 @@ function detectMove(move) {
         }
         break;
       case 'up':
-        stepMusic.play()
+        stepMusic.play();
         if (player.y <= 0) {
           console.log('Border');
         } else {
@@ -335,7 +370,7 @@ function detectMove(move) {
         }
         break;
       case 'down':
-        stepMusic.play()
+        stepMusic.play();
         if (player.y === 440) {
           console.log('Border');
         } else {
@@ -349,18 +384,18 @@ function detectMove(move) {
 document.body.onkeydown = function (e) {
   switch (e.keyCode) {
     case 38:
-      direction = 'up'
+      direction = 'up';
       break;
     case 40:
-      direction = 'down'
+      direction = 'down';
       break;
     case 37:
       // left
-      direction = 'left'
+      direction = 'left';
       break;
     case 39:
       //right
-      direction = 'right'
+      direction = 'right';
       break;
     default:
       break;
@@ -377,8 +412,7 @@ function detectCollision(obs) {
     a.y + a.height > b.y
   ) {
     // collision detected!
-    switch(direction)
-    {
+    switch (direction) {
       case 'left':
         if (player.x <= 0) {
           console.log('Border');
@@ -397,7 +431,7 @@ function detectCollision(obs) {
         if (player.y <= 0) {
           console.log('Border');
         } else {
-            player.y += 5;
+          player.y += 5;
         }
         break;
       case 'down':
@@ -407,11 +441,11 @@ function detectCollision(obs) {
           player.y -= 5;
         }
         break;
-    }  
+    }
   }
   // });
 }
-function detectVirusObsCollision (obs) {
+function detectVirusObsCollision(obs) {
   var a = { x: obs.x, y: obs.y, width: obs.w, height: obs.h }; //Our obstacles
   var b = { x: player.x, y: player.y, width: player.w, height: player.h }; //Our player
   if (
@@ -421,12 +455,13 @@ function detectVirusObsCollision (obs) {
     a.y + a.height > b.y
   ) {
     // collision detected!
-    lives--
-    virus.splice(obs,1)
+    lives--;
+    tracker()
+    virus.splice(obs, 1);
   }
 }
 function detectVirusCollision() {
-  var a = { x: 0, y: 0, width: -25+virusCount, height: 500 }; //Our virus
+  var a = { x: 0, y: 0, width: -25 + virusCount, height: 500 }; //Our virus
   var b = { x: player.x, y: player.y, width: player.w, height: player.h }; //Our player
   if (
     a.x < b.x + b.width &&
@@ -435,16 +470,16 @@ function detectVirusCollision() {
     a.y + a.height > b.y
   ) {
     // collision detected!
-    contamination++
-    if(contamination === 200)
-    {
-      lives--
-      contamination = 0
+    contamination++;
+    if (contamination === 200) {
+      lives--;
+      tracker()
+      contamination = 0;
     }
-}
+  }
 }
 function detectWin() {
-  var a = { x: 699, y: win.y+25, width: 1, height: 1 }; //Our obstacles
+  var a = { x: 699, y: win.y + 25, width: 1, height: 1 }; //Our obstacles
   var b = { x: player.x, y: player.y, width: player.w, height: player.h }; //Our car
   if (
     a.x < b.x + b.width &&
@@ -452,124 +487,116 @@ function detectWin() {
     a.y < b.y + b.height &&
     a.y + a.height > b.y
   ) {
-      // collision detected!
-      playingMusic.pause()
-      successMusic.play()
-      gameTracker.level++
-      newLevel = true;
-      win.w = 0;
-      win.h = 0;
-     setInterval(() => {
-      playingMusic.play()
-     }, 1700);
-    }
+    // collision detected!
+    playingMusic.pause();
+    successMusic.play();
+    gameTracker.level++;
+    newLevel = true;
+    win.w = 0;
+    win.h = 0;
+    setInterval(() => {
+      playingMusic.play();
+    }, 1700);
+  }
 }
 function drawLives() {
-  ctx.fillStyle = 'white'
-  ctx.font = '20px serif'
-  ctx.fillText(lives.toString(),player.x+17,player.y)
+  ctx.fillStyle = 'white';
+  ctx.font = '20px serif';
+  ctx.fillText(lives.toString(), player.x + 17, player.y);
 }
+
+function drawName() {
+  ctx.fillStyle = 'white';
+  ctx.font = '14px serif';
+  ctx.fillText((gameTracker.name).toString(), player.x+3, player.y-15);
+}
+
 function drawContamination() {
-  ctx.fillStyle = 'red'
-  ctx.fillRect(player.x,player.y-10,40,10)
-  ctx.fillStyle = 'green'
-  ctx.fillRect(player.x,player.y-10,(40- 40*(contamination/200)),10)
+  ctx.fillStyle = 'red';
+  ctx.fillRect(player.x, player.y - 10, 40, 10);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(player.x, player.y - 10, 40 - 40 * (contamination / 200), 10);
 }
 function startGame() {
   ctx.clearRect(0, 0, 700, 500);
-  tracker()
   
-  stepMusic.pause()
+  tracker();
 
-drawCanvas();
+  stepMusic.pause();
+
+  drawCanvas();
 
   drawVirus(virusCount);
 
-  for(i=0;i<obstacle.length;i++)
-  {
+  for (i = 0; i < obstacle.length; i++) {
     drawObstacles(obstacle[i]);
   }
 
-  for(j=0;j<virus.length;j++)
-  {
-    drawVirusObs(virus[j])
+  for (j = 0; j < virus.length; j++) {
+    drawVirusObs(virus[j]);
   }
   winningShade();
   step();
-  if (lightCounter % lightTime === lightTime-1) {
+  if (lightCounter % lightTime === lightTime - 1) {
     lightSwitch = !lightSwitch;
   }
   if (lightSwitch) {
     lightsOff();
     lightTime = 500;
-    virusCount+= .5;
+    virusCount += 0.5;
     detectWin();
     detectMove(canMove);
     detectVirusCollision();
-    for(i=0;i<obstacle.length;i++)
-    {
-      detectCollision(obstacle[i])
+    for (i = 0; i < obstacle.length; i++) {
+      detectCollision(obstacle[i]);
     }
-    for(j=0;j<virus.length;j++)
-    {
-      detectVirusObsCollision(virus[j])
+    for (j = 0; j < virus.length; j++) {
+      detectVirusObsCollision(virus[j]);
     }
-    direction = ''
+    direction = '';
   } else {
     lightTime = 100;
   }
   lightCounter++;
   // drawCar();
   drawPlayer();
-  drawLives();
+  drawName();
+  // drawLives();
   drawContamination();
-  if(newLevel)
-  {
-    virusCount = 0
-    lightSwitch = false
-    lightCounter = 0
+  if (newLevel) {
+    virusCount = 0;
+    lightSwitch = false;
+    lightCounter = 0;
     //clear obstacle array
-    for(i=0;i<obstacle.length;i++)
-    {
-      deleteObstacle(i)
+    for (i = 0; i < obstacle.length; i++) {
+      deleteObstacle(i);
     }
-    for(j=0;j<virus.length;j++)
-    {
-      deleteVirus(j)
+    for (j = 0; j < virus.length; j++) {
+      deleteVirus(j);
     }
-    canMove = false
-    if(player.x > 0)
-    {
-      player.x -= 10
-    }
-    else
-    {
+    canMove = false;
+    if (player.x > 0) {
+      player.x -= 10;
+    } else {
       //create new obstacle array
       createObstacles();
       createVirus();
-      canMove = true
-      newLevel = false
-      win.y = Math.floor(Math.random()*400 + 50)
-      win.h = 50
-      win.w = 50
+      canMove = true;
+      newLevel = false;
+      win.y = Math.floor(Math.random() * 400 + 50);
+      win.h = 50;
+      win.w = 50;
       lightCounter = 0;
     }
   }
-  if(lives <= 0)
-
-  {
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0,0,700,500)
-    ctx.fillStyle = "white"
-    ctx.font = '50px serif'
-    ctx.fillText('Game Over',250,245)
-    playingMusic.pause()
-    gameOver.play()
-   
+  if (lives <= 0) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, 700, 500);
+    ctx.fillStyle = 'white';
+    ctx.font = '50px serif';
+    ctx.fillText('Game Over', 250, 245);
+    playingMusic.pause();
+    gameOver.play();
   }
   animateId = window.requestAnimationFrame(startGame); //Game rendering -infinite loop that goes super fast
 }
-
-
-
-
